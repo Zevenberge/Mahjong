9b77c2d9511fd245a07dd99b3b152f82eb0b928e
Zevenberge <nanayamae@gmail.com>
2016-06-18 18:02:55 +0200

    Single player controller created
    The single player controller uses its yield function to check on the AI.

--- source/engine/ai.d
+++ source/engine/ai.d
@@ -1,51 +1,65 @@
 module mahjong.engine.ai;
 
-import std.stdio;
 import std.random;
+import std.uuid;
 
+import mahjong.engine.enums.game;
+import mahjong.engine.gamefront;
 import mahjong.engine.mahjong;
-import mahjong.domain.tile;
 
-class aiRandom
+class AI
 {
-  Tile Discard(ref Tile[] hand)
-  {
-    ulong discard = uniform(0,hand.length);
-    return hand[discard];
-  }
-
-  bool wantToPon(ref Tile[] hand, const ref Tile discard)
-  {
-    return false;
-  }
-
-  bool wantToRon(ref Tile[] hand, const ref Tile discard)
-  {
-    return true;
-  }
+	void interact()
+	{
+		final switch(_front.requiredInteraction) with (Interaction)
+		{
+			case Draw:
+				draw;
+				break;
+			case Discard:
+				discard;
+				break;
+			case None:
+				return;
+		}
+	}
+	
+	protected:
+		this(GameFront front)
+		{
+			_front = front;
+		}
+		GameFront _front;
+	
+		abstract void draw();
+		abstract void discard();
 }
 
-class aiKyari
+class RandomAI : AI
 {
-  Tile Discard(ref Tile[] hand)
-  {
-    ulong discard;
-    size_t handSize = hand.length;
-    ulong[] checkedTiles;
-    foreach(tile; hand)
-    {
-
-    }
-    return hand[discard];
-  }
-
-  bool wantToPon(ref Tile[] hand, const ref Tile discard)
-  {
-    return true;
-  }
-
-  bool wantToRon(ref Tile[] hand, const ref Tile discard)
-  {
-    return true;
-  }
+	this(GameFront front)
+	{
+		super(front);
+	}
+	
+	protected override void draw()
+	{
+		_front.draw;
+	}
+	
+	protected override void discard()
+	{
+		auto player = _front.owningPlayer;
+		if(player.isMahjong)
+		{
+			_front.tsumo;
+		}
+		else
+		{
+			auto hand = player.game.closedHand.tiles;
+			auto discard = uniform(0, hand.length);
+			_front.discard(hand[discard].id);
+		}
+	}
 }
+
---
+++ source/graphics/controllers/game/singleplayercontroller.d
@@ -0,0 +1,36 @@
+module mahjong.graphics.controllers.game.singleplayercontroller;
+
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.experimental.logger;
+import dsfml.graphics;
+import mahjong.engine.ai;
+import mahjong.engine.gamefront;
+import mahjong.graphics.controllers.game.gamecontroller;
+
+class SinglePlayerController : GameController
+{
+	this(RenderWindow window, GameFront[] gameFronts)
+	{
+		trace("Constructing single player controller.");
+		super(window, gameFronts);
+		initialiseAI;
+	}
+	
+	override void yield()
+	{
+		foreach_reverse(ai; _ais)
+		{
+			ai.interact;
+		}
+	}
+	
+	private void initialiseAI()
+	{
+		trace("Initialising AI");
+		_ais = _gameFronts[1..$].map!(f => new RandomAI(f).to!AI).array;
+		trace("Initialised AI");
+	}
+	private AI[] _ais;
+}
