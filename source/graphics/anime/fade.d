ee2ff282be2f9ac7631db4bfef213c104515c620
Zevenberge <nanayamae@gmail.com>
2017-06-14 21:21:14 +0200

    Tests for the fade animations added

--- source/graphics/anime/fade.d
+++ source/graphics/anime/fade.d
@@ -1,6 +1,5 @@
 module mahjong.graphics.anime.fade;
 
-import std.algorithm;
 import std.conv;
 import dsfml.graphics;
 import mahjong.graphics.anime.animation;
@@ -32,6 +31,19 @@ class AppearTextAnimation : Animation
 	}
 }
 
+unittest
+{
+	import mahjong.graphics.cache.font;
+	auto text = new Text;
+	text.setColor(Color(255,255,255,1));
+	auto animation = new AppearTextAnimation(text, 2);
+	animation.animate;
+	assert(text.getColor.a == 128, "The text should have appeared half");
+	animation.animate;
+	assert(text.getColor.a == 255, "The text should have fully appeared");
+	assert(animation.done, "The animation should be done");
+}
+
 class FadeSpriteAnimation : Animation
 {
 	this(Sprite sprite, int amountOfFrames)
@@ -55,4 +67,16 @@ class FadeSpriteAnimation : Animation
 	{
 		return _amountOfFrames == 0;
 	}
+}
+
+unittest
+{
+	auto sprite = new Sprite;
+	sprite.color = Color(255, 255, 255, 254);
+	auto animation = new FadeSpriteAnimation(sprite, 2);
+	animation.animate;
+	assert(sprite.color.a == 127, "The sprite should be half-gone");
+	animation.animate;
+	assert(sprite.color.a == 0, "The sprite should be gone");
+	assert(animation.done, "The fade should be over");
 }
