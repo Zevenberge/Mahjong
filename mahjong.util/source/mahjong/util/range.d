245109760579943932c48eb70e0e17ca1c502959
Marco de Wild <mdwild@sogyo.nl>
2019-04-26 08:08:05 +0200

    Reformatted document

--- mahjong.util/source/mahjong/util/range.d
+++ mahjong.util/source/mahjong/util/range.d
@@ -9,20 +9,19 @@ import std.traits;
 
 template max(alias pred, TReturn)
 {
-	auto max(Range)(Range range) 
-		if(isInputRange!Range && isNumeric!TReturn)
+	auto max(Range)(Range range) if (isInputRange!Range && isNumeric!TReturn)
 	{
 		auto myMax = TReturn.init;
-		foreach(element; range)
+		foreach (element; range)
 		{
 			auto value = pred(element);
-			if(value > myMax)
+			if (value > myMax)
 			{
 				myMax = value;
 			}
-			static if(isFloatingPoint!TReturn)
+			static if (isFloatingPoint!TReturn)
 			{
-				if(myMax.isNaN)
+				if (myMax.isNaN)
 				{
 					myMax = value;
 				}
@@ -31,26 +30,31 @@ template max(alias pred, TReturn)
 		return myMax;
 	}
 }
+
 unittest
 {
-	assert([0,1,2,3,4,5].max!(s => s % 5, int) == 4, "It takes the max of the evaluated expression");
-	assert([0f,1f,2f,3f,4f,5f].max!(s => s % 5, float) == 4, "It should work for floats");
-	assert([0f,1f,2f,3f,float.nan,5f].max!(s => s % 5, float) == 3, "The max function should ignore NaN");
-	assert([0f,3f,2f,float.nan,1f,5f].max!(s => s % 5, float) == 3, "The max function is independent on order");
+	assert([0, 1, 2, 3, 4, 5].max!(s => s % 5, int) == 4,
+			"It takes the max of the evaluated expression");
+	assert([0f, 1f, 2f, 3f, 4f, 5f].max!(s => s % 5, float) == 4, "It should work for floats");
+	assert([0f, 1f, 2f, 3f, float.nan, 5f].max!(s => s % 5, float) == 3,
+			"The max function should ignore NaN");
+	assert([0f, 3f, 2f, float.nan, 1f, 5f].max!(s => s % 5, float) == 3,
+			"The max function is independent on order");
 }
 
 T remove(alias pred, T)(ref T[] array, const T element)
 {
-    foreach(i, e; array)
-    {
-        if(pred(e, element))
-        {
-            array = array[0 .. i] ~ array [i+1..$];
-            return e;
-        }
-    }
+	foreach (i, e; array)
+	{
+		if (pred(e, element))
+		{
+			array = array[0 .. i] ~ array[i + 1 .. $];
+			return e;
+		}
+	}
 	return T.init;
 }
+
 unittest
 {
 
@@ -66,34 +70,33 @@ template without(alias equality = (a, b) => a == b)
 
 template first(alias pred)
 {
-	auto first(Range)(Range range) if(isInputRange!Range)
+	auto first(Range)(Range range) if (isInputRange!Range)
 	{
 		auto foundRange = range.find!pred;
-		if(foundRange.empty) return (ElementType!Range).init;
+		if (foundRange.empty)
+			return (ElementType!Range).init;
 		return foundRange.front;
 	}
 }
 
-size_t indexOf(Range, E)(Range range, E element)
-	if(isInputRange!Range)
+size_t indexOf(Range, E)(Range range, E element) if (isInputRange!Range)
 {
 	return range.countUntil!(e => e == element);
 }
 
 unittest
 {
-
 }
 
 T[] insertAt(T)(T[] collection, T element, size_t index)
 {
 	T[] placeholder;
-	if(index != 0)
+	if (index != 0)
 	{
 		placeholder ~= collection[0 .. index];
 	}
 	placeholder ~= element;
-	if(index != collection.length)
+	if (index != collection.length)
 	{
 		placeholder ~= collection[index .. $];
 	}
@@ -103,16 +106,15 @@ T[] insertAt(T)(T[] collection, T element, size_t index)
 
 unittest
 {
-	assert([0,1,2] == [1,2].insertAt(0,0), "New element should be inserted at 0");
-	assert([1,0,2] == [1,2].insertAt(0,1), "New element should be inserted at 1");
-	assert([1,2,0] == [1,2].insertAt(0,2), "New element should be inserted at 2");
+	assert([0, 1, 2] == [1, 2].insertAt(0, 0), "New element should be inserted at 0");
+	assert([1, 0, 2] == [1, 2].insertAt(0, 1), "New element should be inserted at 1");
+	assert([1, 2, 0] == [1, 2].insertAt(0, 2), "New element should be inserted at 2");
 }
 
-
-template sum(fun...) if(fun.length >= 1)
+template sum(fun...) if (fun.length >= 1)
 {
 	alias yeOldeSum = std.algorithm.sum;
-	auto sum(Range)(Range range) if(isInputRange!(Unqual!Range))
+	auto sum(Range)(Range range) if (isInputRange!(Unqual!Range))
 	{
 		return yeOldeSum(range.map!fun);
 	}
@@ -131,18 +133,19 @@ unittest
 
 template flatMap(alias fun) //if(isInputRange!(ReturnType!fun))
 {
-	auto flatMap(Range)(Range range) if(isInputRange!Range)
+	auto flatMap(Range)(Range range) if (isInputRange!Range)
 	{
-        auto mappedResult = range.map!fun;
-        static if(is(ElementType!(typeof(mappedResult)) == E[], E))
-        {
-            if(mappedResult.empty) return null;
-            return fold!((a, b) => a ~ b)(mappedResult);
-        }
-        else
-        {
-            return joiner(mappedResult);
-        }
+		auto mappedResult = range.map!fun;
+		static if (is(ElementType!(typeof(mappedResult)) == E[], E))
+		{
+			if (mappedResult.empty)
+				return null;
+			return fold!((a, b) => a ~ b)(mappedResult);
+		}
+		else
+		{
+			return joiner(mappedResult);
+		}
 	}
 }
 
@@ -152,7 +155,8 @@ unittest
 	{
 		int[] ints;
 	}
-	auto flattened = [Bubble([1,2]), Bubble([3,4])].flatMap!(x => x.ints).array;
+
+	auto flattened = [Bubble([1, 2]), Bubble([3, 4])].flatMap!(x => x.ints).array;
 	assert([1, 2, 3, 4].equal(flattened), "The two arrays should be joined");
 }
 
@@ -162,39 +166,44 @@ unittest
 	{
 		int[] ints;
 	}
+
 	Bubble[] bubbles; // Empty range;
 	auto flattened = bubbles.flatMap!(x => x.ints).array;
-	assert(flattened.length == 0, "Flat-mapping an empty range should return an empty range of the result type");
+	assert(flattened.length == 0,
+			"Flat-mapping an empty range should return an empty range of the result type");
 }
 
 unittest
 {
-    import std.range : iota;
-    import fluent.asserts;
-    struct Counter
-    {
-        auto oneTwoThree()
-        {
-            return iota(1, 4);
-        }
-    }
-    auto counters = [Counter(), Counter(), Counter()].flatMap!(c => c.oneTwoThree);
-    counters.should.equal([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+	import std.range : iota;
+	import fluent.asserts;
+
+	struct Counter
+	{
+		auto oneTwoThree()
+		{
+			return iota(1, 4);
+		}
+	}
+
+	auto counters = [Counter(), Counter(), Counter()].flatMap!(c => c.oneTwoThree);
+	counters.should.equal([1, 2, 3, 1, 2, 3, 1, 2, 3]);
 }
 
 template atLeastOneUntil()
 {
-    auto atLeastOneUntil(Range, Needle)(Range range, Needle needle)
-    {
-        static assert(isInputRange!Range, 
-            "An input range should be supplied instead of " ~ Range.stringof);
-        bool isOnePassed = false;
-        return range.until!((x, y) {
-                if(isOnePassed) return x == y;
-                isOnePassed = true;
-                return false;
-            })(needle);
-    }
+	auto atLeastOneUntil(Range, Needle)(Range range, Needle needle)
+	{
+		static assert(isInputRange!Range,
+				"An input range should be supplied instead of " ~ Range.stringof);
+		bool isOnePassed = false;
+		return range.until!((x, y) {
+			if (isOnePassed)
+				return x == y;
+			isOnePassed = true;
+			return false;
+		})(needle);
+	}
 }
 
 unittest
