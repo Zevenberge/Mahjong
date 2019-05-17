b005f31c27ee28f64b350dff7353a1ce2294b70f
Marco de Wild <mdwild@sogyo.nl>
2019-05-17 15:53:07 +0200

    Some more tests for betterai

--- mahjong.ai/source/mahjong/ai/advanced.d
+++ mahjong.ai/source/mahjong/ai/advanced.d
@@ -26,7 +26,9 @@ TurnDecision discardUnrelatedTile(const Hand hand, const Player player) pure @no
     Rebindable!(const Tile) tile;
     static foreach(check; AliasSeq!(
         selectOnlyTileOfType,
-        selectLonelyHonour
+        selectLonelyHonour,
+        selectUnconnectedTerminal,
+        selectUnconnectedTile
         ))
     {
         tile = check(hand);
@@ -89,6 +91,70 @@ unittest
     result.selectedTile.should.equal(h.tiles[8]);
 }
 
+@("If I have an unconnected starting terminal, I want to discard it")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀔🀕🀘🀘🀙🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.should.equal(h.tiles[10]);
+}
+
+@("If I have an unconnected simple, I want to discard it")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀔🀕🀘🀘🀘🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.should.equal(h.tiles[11]);
+}
+
+@("If I have an unconnected simple, I want to discard it even if it's the last one")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀔🀕🀘🀘🀘🀜🀜🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.should.equal(h.tiles[13]);
+}
+
+@("If I have an unconnected simple, I want to discard it even if it's in the middle")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀕🀘🀘🀘🀜🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.should.equal(h.tiles[6]);
+}
+
+@("If I have a set with a double end, they are actually unconnected, so I want to discard the double")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀖🀗🀘🀘🀜🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.shouldBeEither(h.tiles[8], h.tiles[9]);
+}
+
+@("If I have a set with a double in the middle, I want to discard the double")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀖🀗🀗🀘🀜🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.shouldBeEither(h.tiles[7], h.tiles[8]);
+}
+
+@("If I have a set with a double at the start, I want to discard the double")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀖🀖🀗🀘🀜🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.shouldBeEither(h.tiles[6], h.tiles[7]);
+}
+
+@("If I have a set of 4, they are actually unconnected, so I want to discard either end")
+unittest
+{
+    auto h = hand("🀀🀀🀀🀒🀒🀒🀕🀖🀗🀘🀜🀜🀠🀠"d);
+    auto result = discardUnrelatedTile(h, player);
+    result.selectedTile.shouldBeEither(h.tiles[6], h.tiles[9]);
+}
+
 private const(Tile) selectOnlyTileOfType(const Hand hand) pure @nogc nothrow
 {
     foreach(set; hand.tilesByType)
@@ -117,7 +183,97 @@ private const(Tile) selectLonelyHonour(const Hand hand) pure @nogc nothrow
     return null;
 }
 
+private const(Tile) selectUnconnectedTerminal(const Hand hand) pure @nogc nothrow
+{
+    foreach(set; hand.nonHonoursByType)
+    {
+        Rebindable!(const Tile) first;
+        Rebindable!(const Tile) second = set.front;
+        set.popFront;
+        foreach(s; set)
+        {
+            first = second;
+            if(first.isTerminal)
+            {
+                if(!first.areConnected(s)) return first;
+            }
+            second = s;
+        }
+        if(second && second.isTerminal)
+        {
+            if(!first.areConnected(second)) return second;
+        }
+    }
+    return null;
+}
+
+private const(Tile) selectUnconnectedTile(const Hand hand) pure @nogc nothrow
+{
+    foreach(set; hand.nonHonoursByType)
+    {
+        Rebindable!(const Tile) first;
+        Rebindable!(const Tile) second = set.front;
+        set.popFront;
+        if(set.empty)
+        {
+            return second;
+        }
+        Rebindable!(const Tile) third = set.front;
+        if(!second.areConnected(third))
+        {
+            return second;
+        }
+        set.popFront;
+        bool iterated = false;
+        foreach(tile; set)
+        {
+            iterated = true;
+            first = second;
+            second = third;
+            third = tile;
+            if(!first.areConnected(second)
+                && !second.areConnected(third))
+            {
+                return second;
+            }
+            
+        }
+        if(iterated &&!second.areConnected(third))
+        {
+            return third;
+        }
+    }
+    return null;
+}
+
+private const(Tile) selectTileNotConnectedFromSet(const Hand hand) pure @nogc nothrow
+{
+    foreach(set; hand.nonHonoursByType)
+    {
+        
+    }
+    return null;
+}
+
 private TurnDecision discard(const Tile tile, const Player player) pure @nogc nothrow
 {
     return TurnDecision(player, TurnDecision.Action.discard, tile);
+}
+
+version(mahjong_test)
+{
+    void shouldBeEither(T, U...)(T actual, U expected, 
+        string file = __FILE__, size_t line = __LINE__)
+    {
+        import std.algorithm;
+        import std.conv;
+        import fluent.asserts;
+        if(![expected].any!(exp => exp is actual))
+        {
+            IResult message = new MessageResult("Expected the value to be either of the supplied values.");
+            IResult source = new SourceResult(file, line);
+            IResult result = new ExpectedActualResult([expected].to!string, actual.to!string);
+            throw new TestException([result, source], file, line);
+        }
+    }
 }
