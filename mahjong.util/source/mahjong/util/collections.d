0b20f8a2d7dca2e5f3f529d337d9b396a9b32544
Marco de Wild <mdwild@sogyo.nl>
2019-05-17 15:52:53 +0200

    Created first attempt at nogcarray

--- mahjong.util/source/mahjong/util/collections.d
+++ mahjong.util/source/mahjong/util/collections.d
@@ -33,3 +33,143 @@ unittest
     numbers ~= 42;
     numbers.values.should.equal([42]);
 }
+
+struct NoGcArray(size_t maxSize, T)
+{
+    private T[maxSize] _buffer;
+    private size_t _length;
+    size_t length() pure const @nogc nothrow
+    {
+        return _length;
+    } 
+
+    void opOpAssign(string op)(T element) pure @nogc nothrow
+    in(_length < maxSize, "Cannot append if the buffer is fully filled.")
+    {
+        static if(op == "~")
+        {
+            _buffer[_length] = element;
+            ++_length;
+        }
+        else
+        {
+            static assert(false, "Only concatenation supported");
+        }
+    }
+
+    T opIndex(size_t index)
+    in(index < _length, "Cannot access index greater than length")
+    {
+        return _buffer[index];
+    }
+
+    private size_t _index;
+
+    T front() pure @nogc nothrow
+    {
+        return this[_index];
+    }
+
+    void popFront() pure @nogc nothrow
+    {
+        _index++;
+    }
+
+    bool empty() pure @nogc nothrow
+    {
+        return _length <= _index;
+    }
+}
+
+@("Is a no gc array initialised empty?")
+unittest
+{
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array.length.should.equal(0);
+}
+
+@("Can I add to a no gc array")
+unittest
+{
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array ~= 42;
+    array.length.should.equal(1);
+    array[0].should.equal(42);
+}
+
+@("Can I append two elements to a no gc array?")
+unittest
+{
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array ~= 42;
+    array ~= 420;
+    array.length.should.equal(2);
+    array[0].should.equal(42);
+    array[1].should.equal(420);
+}
+
+@("Can I append beyond max size?")
+unittest
+{
+    import fluent.asserts;
+    NoGcArray!(1, int) array;
+    array ~= 4;
+    (array ~= 420).should.throwSomething;
+}
+
+@("Can I ask for elements outside of the length?")
+unittest
+{
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    (array[0]).should.throwSomething;
+}
+
+@("Can I use algorithm functions on a no gc array")
+unittest
+{
+    import std.algorithm : sum;
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array.sum.should.equal(0);
+    array ~= 420;
+    array ~= 42;
+    array.sum.should.equal(462);
+}
+
+@("Is the length of the no gc array walked correctly")
+unittest
+{
+    import std.range : walkLength;
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array.walkLength.should.equal(0);
+    array ~= 42;
+    array.walkLength.should.equal(1);
+}
+
+@("Does chaining stuff work correctly?")
+unittest
+{
+    import std.algorithm : sum, map;
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array ~= 420;
+    array ~= 42;
+    array.map!(x => x*2).sum.should.equal(924);
+}
+
+@("An operation should not be destructive")
+unittest
+{
+    import std.algorithm : sum, map;
+    import fluent.asserts;
+    NoGcArray!(4, int) array;
+    array ~= 420;
+    array ~= 42;
+    array.map!(x => x*2).sum.should.equal(924);
+    array.map!(x => x*2).sum.should.equal(924);
+}
