18de3d84d0df6b73b5e863f4ccd22f93d7a319ee
Marco de Wild <mdwild@sogyo.nl>
2019-07-12 16:10:54 +0200

    Extra keys added

--- mahjong.test/source/mahjong/test/key.d
+++ mahjong.test/source/mahjong/test/key.d
@@ -9,3 +9,31 @@ Event returnKeyPressed()
     keyEvent.key = Event.KeyEvent(Keyboard.Key.Return, false, false, false, false);
     return keyEvent;
 }
+
+Event upKeyPressed()
+{
+    Event keyEvent = Event(Event.EventType.KeyReleased);
+    keyEvent.key = Event.KeyEvent(Keyboard.Key.Up, false, false, false, false);
+    return keyEvent;
+}
+
+Event downKeyPressed()
+{
+    Event keyEvent = Event(Event.EventType.KeyReleased);
+    keyEvent.key = Event.KeyEvent(Keyboard.Key.Down, false, false, false, false);
+    return keyEvent;
+}
+
+Event escapeKeyPressed()
+{
+    Event keyEvent = Event(Event.EventType.KeyReleased);
+    keyEvent.key = Event.KeyEvent(Keyboard.Key.Escape, false, false, false, false);
+    return keyEvent;
+}
+
+Event gKeyPressed()
+{
+    Event keyEvent = Event(Event.EventType.KeyReleased);
+    keyEvent.key = Event.KeyEvent(Keyboard.Key.G, false, false, false, false);
+    return keyEvent;
+}
